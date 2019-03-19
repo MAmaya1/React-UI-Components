8fff7ae0918b821bff468f8aabd46c6c1b7e7cf0
Mario Amaya <thefozcat@gmail.com>
2019-03-19 14:39:57 -0700

    updated App.js with .map methods

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,6 +4,9 @@ import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
+const buttonNums = ['7', '8', '9', '4', '5', '6', '1', '2', '3'];
+const operators = ['÷', 'x', '-', '+', '='];
+
 const App = () => {
   return (
     <div className="calculator">
@@ -11,23 +14,16 @@ const App = () => {
       <div className="calculator-buttons">
         <div className="number-container">
           <ActionButton buttonStyle="action-btn" text="clear"/>
-          <NumberButton buttonStyle="number-btn" text="7"/>
-          <NumberButton buttonStyle="number-btn" text="8"/>
-          <NumberButton buttonStyle="number-btn" text="9"/>
-          <NumberButton buttonStyle="number-btn" text="4"/>
-          <NumberButton buttonStyle="number-btn" text="5"/>
-          <NumberButton buttonStyle="number-btn" text="6"/>
-          <NumberButton buttonStyle="number-btn" text="1"/>
-          <NumberButton buttonStyle="number-btn" text="2"/>
-          <NumberButton buttonStyle="number-btn" text="3"/>
+          {buttonNums.map(number => {
+              return <NumberButton buttonStyle="number-btn" text={number}/>;
+            }
+          )}
           <ActionButton buttonStyle="action-btn" text="0"/>
         </div>
         <div className="operator-buttons">
-          <NumberButton buttonStyle="operator-btn" text="÷"/>
-          <NumberButton buttonStyle="operator-btn" text="x"/>
-          <NumberButton buttonStyle="operator-btn" text="-"/>
-          <NumberButton buttonStyle="operator-btn" text="+"/>
-          <NumberButton buttonStyle="operator-btn" text="="/>
+          {operators.map(operator => {
+            return <NumberButton buttonStyle="operator-btn" text={operator}/>;
+          })}
         </div>
       </div>
     </div>
