2616a3639b92183c2d9a146d4f506387039721d6
Mario Amaya <thefozcat@gmail.com>
2019-03-18 17:19:37 -0700

    updated header and card styles

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -5,7 +5,7 @@ const CardContent = () => {
     return (
         <div className="card-content">
             <h2 className="card-title">Get started with React</h2>
-            <p>React makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>
+            <p className="card-text">React makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>
             <a href="https://reactjs.org/" className="grey-link">reactjs.org</a>
         </div>
     )
